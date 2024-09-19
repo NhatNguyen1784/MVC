4859f7a5204e1457dfeb2b9283ef919207c5d557
NhatNguyen1784 <nguyennhatnguyen1782004@gmail.com>
2024-09-19 09:47:59 +0700

    change view login

---
+++ src/main/java/vn/hcmute/controllers/RegisterController.java
@@ -0,0 +1,8 @@
+package vn.hcmute.controllers;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+@WebServlet(urlPatterns = {"/register"})
+public class RegisterController extends HttpServlet {
+
+}
